ec679312ba474a0703c8562a3a1c5df68fe80e2f
Mahmoud Arasteh <arastehnasab@gmail.com>
2025-04-16 16:00:16 -0700

    Increment version

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Canonicalize images addresses
 // @namespace    https://arasteh.studio/get-big-images/
-// @version      1.004
+// @version      1.005
 // @description  Load images in the highest resolution available.
 // @author       @ebraminio @arasteh
 // @downloadURL  https://raw.githubusercontent.com/Arasteh/getbigimages/refs/heads/main/script.js
